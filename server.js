f8ba73fee56521e3030daf42b94db4f8bb8f80b3
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-03-28 13:59:05 +0200

    adding routes for articles and tags

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ const PORT = process.env.PORT || 3000;
 const mongoDB = "";
 // mongoose.connect(mongoDB);
 
-app.get('/',(req,res)=>{
-    res.send("Server is running and listening to requests.")
-})
+// app.get('/',(req,res)=>{
+//     res.send("Server is running and listening to requests.")
+// })
+
+app.get("/articles", (req,res)=>{})
+app.get("/articles/:id", (req,res)=>{})
+app.post("/articles", (req,res)=>{})
+app.delete("/articles/:id", (req,res)=>{})
+app.get("/tags", (req,res)=>{})
+app.get("/tags/:id", (req,res)=>{})
+app.post("/tags/:id", (req,res)=>{})
+app.delete("/tags/:id", (req,res)=>{})
+
 
 app.listen(PORT, () => {
     console.warn(`App listening on http://localhost:${PORT}`);
