08a43c1c776d30ccc91bf5f8cfcc9b842e1008b4
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-03-30 18:23:17 +0200

    adding try catch with logging errorhandler

--- server.js
+++ server.js
@@ -18,13 +18,22 @@ app.get('/',(req,res) => {
 
 //Articles
 app.get("/articles", (req,res)=>{
-    Article.find({}, (err, data) => res.send(data))
+    try{
+        Article.find({}, (err, data) => res.send(data))
+    }catch(error){
+        console.log(error)
+    }
 })
 app.get("/articles/:_id", (req,res)=>{
-    Article.find({_id: req.params._id}, (err, data) => res.send(data));
+    try{
+        Article.find({_id: req.params._id}, (err, data) => res.send(data));
+    }catch(error){
+        console.log(error)
+    }
 })
 app.post("/articles", (req,res)=>{
-    const newArticle = req.body;
+    try{
+        const newArticle = req.body;
         Article.create({
             // id: newArticle.id,
             title: newArticle.title,
@@ -37,91 +46,121 @@ app.post("/articles", (req,res)=>{
         }).then(function(newArticles){
             res.send(newArticles);
         })
-    
+    }catch(error){
+        console.log(error)
+    }
    
 })
 app.put("/articles/:_id", (req,res) => {
-    const newArticle = req.body;
-    Article.updateOne({_id: req.params._id},{$set:{
-        // id: newArticle.id,
-        title: newArticle.title,
-        body: newArticle.body,
-        tags: newArticle.tags,
-        url: newArticle.url,
-        ext: newArticle.ext,
-        visible: newArticle.visible,
-        updatedDate: Date.now(),   
-    }}).then(function(newArticles){
-        res.send(newArticles);})
+    try{
+        const newArticle = req.body;
+        Article.updateOne({_id: req.params._id},{$set:{
+            // id: newArticle.id,
+            title: newArticle.title,
+            body: newArticle.body,
+            tags: newArticle.tags,
+            url: newArticle.url,
+            ext: newArticle.ext,
+            visible: newArticle.visible,
+            updatedDate: Date.now(),   
+        }}).then(function(newArticles){
+            res.send(newArticles);})
+    }catch(error){
+        console.log(error)
+    }
 })
-app.delete("/articles/:id", (req,res)=>{
-    Article.deleteOne({ _id: req.params._id }).then(function () {
-        res.end();
-      });
+app.delete("/articles/:_id", (req,res)=>{
+    try{
+        Article.deleteOne({ _id: req.params._id }).then(function () {
+            res.end();
+        });
+    }catch(error){
+        console.log(error)
+    }
 })
 //Search for Articles based on Tags
 app.get("/search/:tags", (req,res)=>{
-    let tagArr = req.params.tags.split(",");
-    console.log(tagArr)
-    Article.find({tags:{$in:[...tagArr]} }, (err, data) => {
-        let arr3hits=[];
-        let arr2hits=[];
-        let arr1hits=[];
-        for(let i = 0; i<data.length;i++){
-            let count = 0;
-            for(let j = 0; j<data[i].tags.length;j++){
-                if(tagArr.includes(data[i].tags[j])){
-                    count +=1;
+    try{
+        let tagArr = req.params.tags.split(",");
+        console.log(tagArr)
+        Article.find({tags:{$in:[...tagArr]} }, (err, data) => {
+            let arr3hits=[];
+            let arr2hits=[];
+            let arr1hits=[];
+            for(let i = 0; i<data.length;i++){
+                let count = 0;
+                for(let j = 0; j<data[i].tags.length;j++){
+                    if(tagArr.includes(data[i].tags[j])){
+                        count +=1;
+                    }
                 }
+                if(count ===3){
+                    arr3hits.push(data[i])
+                } else if(count ===2){
+                    arr2hits.push(data[i])
+                } else {
+                    arr1hits.push(data[i])
+                }
+                
             }
-            if(count ===3){
-                arr3hits.push(data[i])
-            } else if(count ===2){
-                arr2hits.push(data[i])
-            } else {
-                arr1hits.push(data[i])
-            }
-            
-        }
-        let sortedArr = arr3hits.concat(arr2hits).concat(arr1hits)
-        res.send(sortedArr)
-    });
+            let sortedArr = arr3hits.concat(arr2hits).concat(arr1hits)
+            res.send(sortedArr)
+        });
+    }catch(error){
+        console.log(error)
+    }
 })
 
 //Tags
 app.get("/tags", (req,res)=>{
-    Tag.find({}, (err, data) => res.send(data))
+    try{
+        Tag.find({}, (err, data) => res.send(data))
+    }catch(error){
+        console.log(error)
+    }
 })
 app.get("/tags/:name", (req,res)=>{
-    Tag.find({name: req.params.name}, (err, data) => res.send(data));
+    try{
+        Tag.find({name: req.params.name}, (err, data) => res.send(data));
+    }catch(error){
+        console.log(error)
+    }
 })
 app.post("/tags", (req,res)=>{
-    const newTag = req.body;
-    console.log(newTag)
-    Tag.exists({name:newTag.name},(error, result)=>{
-        console.log(JSON.stringify(result).length)
-        if (error){
-            console.log(error)
-        } else {
-            if(JSON.stringify(result).length < 5){
-                Tag.create({
-                            name:newTag.name,
-                            timesClicked: newTag.timesClicked
-                        }).then(function(newTag){
-                            res.send(newTag);
-                        })
+    try{
+        const newTag = req.body;
+        console.log(newTag)
+        Tag.exists({name:newTag.name},(error, result)=>{
+            console.log(JSON.stringify(result).length)
+            if (error){
+                console.log(error)
             } else {
-                Tag.updateOne({name:newTag.name},{$inc:{timesClicked:1}}).then(function (updatedTag) {
-                    res.send(updatedTag);
-                });
-            }             
-        }
-    })
+                if(JSON.stringify(result).length < 5){
+                    Tag.create({
+                                name:newTag.name,
+                                timesClicked: newTag.timesClicked
+                            }).then(function(newTag){
+                                res.send(newTag);
+                            })
+                } else {
+                    Tag.updateOne({name:newTag.name},{$inc:{timesClicked:1}}).then(function (updatedTag) {
+                        res.send(updatedTag);
+                    });
+                }             
+            }
+        })
+    }catch(error){
+        console.log(error)
+    }
 })
 app.delete("/tags/:name", (req,res)=>{
-    Tag.deleteOne({ name: req.params.name }).then(function () {
-        res.end();
-      });
+    try{
+        Tag.deleteOne({ name: req.params.name }).then(function () {
+            res.end();
+        });
+    }catch(error){
+        console.log(error)
+    }
 })
 //Users
 
