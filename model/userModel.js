4abb9d96913ddb2bf19a5aeb746fe1994b9da3c9
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-01 17:08:14 +0200

    Implementing authorisation Routes and methods

--- model/userModel.js
+++ model/userModel.js
@@ -1,76 +1,58 @@
 const mongoose = require("mongoose")
-// const bcrypt = require("bcrypt")
-// const SALT_WORK_FACTOR = 10
+const { isEmail } = require("validator")
+const bcrypt = require("bcrypt")
+const SALT_WORK_FACTOR = 10
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true,"Please enter an E-Mail"],
+        unique: true,
+        lowercase: true,
+        validate : [isEmail,"Please enter a valid E-Mail"]
     },
     password:{
         type: String,
-        required: true,
-        minlength: 8,
-        maxlength: 20
+        required: [true,"Please enter a Password"],
+        minlength: [8,"Password must be min 8 Characters"],
+        maxlength: [20,"Password must be max 20 Characters"]
     }
-}, 
-{ timestamps: true }
+}
 )
 
 // Encryption of user password to save it into database
-// UserSchema.pre("save", function(next)) {
-//     let user = this;
+UserSchema.pre("save", function(next) {
+    let user = this;
         // only hash the password if it has been modified (or is new)
-//   if (!user.isModified('password')) return next();
+  if (!user.isModified('password')) return next();
 
-//   // generate a salt
-//   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//       if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
 
-//       // hash the password using our new salt
-//       bcrypt.hash(user.password, salt, function(err, hash) {
-//           if (err) return next(err);
-//           // override the cleartext password with the hashed one
-//           user.password = hash;
-//           next();
-//       });
-//   });
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
 
-// }
+},
 
 // Authentication
-// UserSchema.statics.authenticate = function (email, password, callback) {
-//     User.findOne({ email: email }).exec(function (err, user) {
-//       if (err) {
-//         return callback(err);
-//       } else if (!user) {
-//         var err = new Error("User not found.");
-//         err.status = 401;
-//         return callback(err);
-//       }
-//       bcrypt.compare(password, user.password, function (err, result) {
-//         if (err) {
-//           return callback(err);
-//         }
-//         if (result === true) {
-//           return callback(null, user);
-//         } else {
-//           var err = new Error("Incorrect password.");
-//           err.status = 402;
-//           return callback(err);
-//         }
-//       });
-//     });
-//   };
+UserSchema.statics.authenticate = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+});
 
 
 const User = mongoose.model("User", UserSchema)
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ config.config();
 const Article = require("./model/articlesModel")
 const Tag = require("./model/tagModel")
 const User = require("./model/userModel")
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 const app = express()
 const PORT = process.env.PORT || 3010;
 const cors = require('cors');
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser());
 const mongoDB = process.env.DB_CONNECT;
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
+    console.warn(`App listening on http://localhost:${PORT}`);}))
+.catch((err) => console.log(err));
 
 app.get('/',(req,res) => {
     res.send("Server is running and listening to requests.")
@@ -23,6 +28,7 @@ app.get("/articles", (req,res)=>{
         Article.find({}, (err, data) => res.send(data))
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.get("/articles/:_id", (req,res)=>{
@@ -30,6 +36,7 @@ app.get("/articles/:_id", (req,res)=>{
         Article.find({_id: req.params._id}, (err, data) => res.send(data));
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.post("/articles", (req,res)=>{
@@ -49,6 +56,7 @@ app.post("/articles", (req,res)=>{
         })
     }catch(error){
         console.log(error)
+        res.send(error)
     }
    
 })
@@ -68,6 +76,7 @@ app.put("/articles/:_id", (req,res) => {
             res.send(newArticles);})
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.delete("/articles/:_id", (req,res)=>{
@@ -77,6 +86,7 @@ app.delete("/articles/:_id", (req,res)=>{
         });
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 //Search for Articles based on Tags
@@ -109,6 +119,7 @@ app.get("/search/:tags", (req,res)=>{
         });
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 
@@ -118,6 +129,7 @@ app.get("/tags", (req,res)=>{
         Tag.find({}, (err, data) => res.send(data))
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.get("/tags/:name", (req,res)=>{
@@ -125,6 +137,7 @@ app.get("/tags/:name", (req,res)=>{
         Tag.find({name: req.params.name}, (err, data) => res.send(data));
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.post("/tags", (req,res)=>{
@@ -152,6 +165,7 @@ app.post("/tags", (req,res)=>{
         })
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 app.delete("/tags/:name", (req,res)=>{
@@ -161,26 +175,115 @@ app.delete("/tags/:name", (req,res)=>{
         });
     }catch(error){
         console.log(error)
+        res.send(error)
     }
 })
 // Users
+function handleErrors(err){
+    console.log(err.message, err.code)
+    let errors = { email:'',password:''}
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
+    //duplicate Errorcode
+    if(err.code === 11000){
+        errors.email = 'That email is already registered'
+        return errors;
+    }
+    //validationerrors
+    if(err.message.includes('User validation failed')){
+        console.log(err)
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors;
+}
+const maxAge = 1 * 24 * 60 * 60; // one day in seconds
+function createToken(id){
+    return jwt.sign({ id }, process.env.SECRET_STRING, {expiresIn:maxAge})
+}
 app
 .get("/user", (req,res)=>{
-
+    try{
+        User.find({}, (err, data) => res.send(data))
+    }catch(error){
+        console.log(error)
+        res.send(error)
+    }
 })
-.get("/user/:id", (req,res)=>{
-
+.get("/user/:_id", (req,res)=>{
+    try{
+        User.find({_id: req.params._id}, (err, data) => res.send(data));
+    }catch(error){
+        console.log(error)
+        res.send(error)
+    }
 })
-.post("/user", (req,res)=>{ // create / registrate new user
-
+.delete("/user/:_id", (req,res)=>{
+    try{
+        User.deleteOne({ _id: req.params._id }).then(function () {
+            res.end();
+        });
+    }catch(error){
+        console.log(error)
+        res.send(error)
+    }
 })
-.delete("/user/:id", (req,res)=>{
-
+.post("/user", async (req,res)=>{ // create / registrate new user
+    const newUser = req.body;
+    try{
+        const user = await User.create({
+            email: newUser.email,
+            password: newUser.password
+        })
+        console.log(user)
+        const token = await createToken(user._id);
+        res.cookie('jwt',token,{maxAge:maxAge * 1000})
+        res.status(201).json({ user: user._id });
+    } catch (error){
+        const errors = handleErrors(error);
+        res.status(400).json({errors})
+    }
 })
-.post("/login", (req, res) => {
+.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+  try {
+    const user = await User.authenticate(email, password);
+    const token = await createToken(user._id);
+    res.cookie('jwt',token,{maxAge:maxAge * 1000})
+    res.status(200).json({ user: user._id });
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({errors})
+  }
   
 })
+.get("/logout", (req,res)=>{
+    res.cookie('jwt','',{maxAge:1})
+    res.send("User successfully logged out")
+})
+.get("/verify", (req,res)=>{
+    const token = req.cookies.jwt;
 
-app.listen(PORT, () => {
-    console.warn(`App listening on http://localhost:${PORT}`);
-  });
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(token, process.env.SECRET_STRING, (err, decodedToken) => {
+      if (err) {
+        res.send(err.message);
+      } else {
+        // console.log(decodedToken);
+        res.send("OK")
+      }
+    });
+  } else {
+    res.send("Not verified")
+  }
+})
