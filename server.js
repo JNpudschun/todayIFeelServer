3a3a476e9d5a4efb440f6e13b606add6581e6f52
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 16:20:11 +0200

    Modifying cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
 app.use(function (req, res, next) {
 
     // // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', "null");
+    res.setHeader('Access-Control-Allow-Origin', null);
     // console.log(req.originalUrl)
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
