48b442638be3d70299f102089a7182d49553d909
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 16:26:38 +0200

    changing acao header

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
 app.use(function (req, res, next) {
 
     // // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', null);
-    // console.log(req.originalUrl)
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
