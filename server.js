aaeae3060ce3cdda64b7bc13601085cb794e02c9
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 16:17:56 +0200

    changing acao

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
 app.use(function (req, res, next) {
 
     // // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-
+    res.setHeader('Access-Control-Allow-Origin', "null");
+    // console.log(req.originalUrl)
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
