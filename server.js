08fe59b576180c129d16d01e75d7944bd7677bea
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 15:16:48 +0200

    Adding credentials to cors config

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 const PORT = process.env.PORT || 3010;
 //setting up the app to use neccesary middleware
 app.use(express.json())
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'http://localhost:3000',credentials:true}));
 app.use(cookieParser());
 //connecting to database and after that setting app to listen on correct port
 mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
