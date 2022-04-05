5b5243fa006596776bfc7542d29a81b85a36d94e
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 16:40:38 +0200

    adding domain to cookie

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.get("/search/:tags", async (req,res)=>{
                 }
                 const date2 = new Date(dateArray[0]+'-'+dateArray[1]+'-'+dateArray[2]+'T00:00:00')
                 const date3 = new Date(date2 - (3600 * 1000 * 22));                
-                res.cookie('voted',true,{expires:date3,httpOnly:true,sameSite:"none",secure:true})
+                res.cookie('voted',true,{expires:date3,httpOnly:true,sameSite:"none",secure:true,domain:"localhost"})
                 res.send(sortedArr)
             }); 
         }
@@ -390,7 +390,7 @@ app.post("/user", async (req,res)=>{
         // console.log(user)
         //create then attach the token to a cookie for the user
         const token = await createToken(user._id);
-        res.cookie('jwt',token,{maxAge:maxAge * 1000,httpOnly:true,sameSite:"none",secure:true})
+        res.cookie('jwt',token,{maxAge:maxAge * 1000,httpOnly:true,sameSite:"none",secure:true,domain:"localhost"})
         //respond with created user id and giving the cookie to logged in user in the process
         res.status(201).json({ user: user._id });
     } catch (error){
@@ -409,7 +409,7 @@ app.post("/login", async (req, res) => {
         // we can continue by creating the token for the verified user
         const token = await createToken(user._id);
         // return the cookie with the jwt token and the userid
-        res.cookie('jwt',token,{maxAge:maxAge * 1000,httpOnly:true,sameSite:"none",secure:true})
+        res.cookie('jwt',token,{maxAge:maxAge * 1000,httpOnly:true,sameSite:"none",secure:true,domain:"localhost"})
         res.status(200).json({ user: user._id });
     } catch (error) {
         const errors = handleErrors(error);
@@ -418,7 +418,7 @@ app.post("/login", async (req, res) => {
 })
 //LOGOUT a user by giving him a cookie of same name thats emoty and expires after 2 millisecong
 app.get("/logout", (req,res)=>{
-    res.cookie('jwt','',{maxAge:1,httpOnly:true,sameSite:"none",secure:true})
+    res.cookie('jwt','',{maxAge:1,httpOnly:true,sameSite:"none",secure:true,domain:"localhost"})
     res.send("User successfully logged out")
 })
 //VERIFY the Token of user trying to access a route that has accescontrol on it
