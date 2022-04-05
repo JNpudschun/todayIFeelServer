f7aaad0e9c0f3a281dad200b9cc7bcb0f1f07da6
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-05 15:34:10 +0200

    modifing cors options

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 const PORT = process.env.PORT || 3010;
 //setting up the app to use neccesary middleware
 app.use(express.json())
-app.use(cors({origin: '',credentials:true}));
+app.use(cors());
 app.use(cookieParser());
 //connecting to database and after that setting app to listen on correct port
 mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
@@ -28,7 +28,7 @@ mongoose.connect(mongoDB).then((result) => app.listen(PORT, () => {
 app.use(function (req, res, next) {
 
     // // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 
     // // Set to true if you need the website to include cookies in the requests sent
     // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
